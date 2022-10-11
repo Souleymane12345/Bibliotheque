b9e72c41946813536924703a1beceaa68e1f6df1
Souley <62396414+Souleymane12345@users.noreply.github.com>
2022-10-11 00:22:27 +0000

    Update settings.py

--- bibliotheque/settings.py
+++ bibliotheque/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'bibliotheque.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-}}
+  DATABASES = {
+    'default': {
+         'ENGINE': 'django.db.backends.oracle',
+         'NAME': 'db_name',
+         'USER': 'username',
+         'PASSWORD': 'password',
+    }
+  }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
